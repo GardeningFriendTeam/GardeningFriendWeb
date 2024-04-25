b8a146fa4e0e4b20505cf5e9213a8a548f4fb739
EvanaSabatella1989 <eva.sabatella@gmail.com>
2024-04-24 22:28:31 -0300

    Creacion de registro de usuario

--- GardeningFriendWeb/frontend/src/app/auth/registro/registro.component.ts
+++ GardeningFriendWeb/frontend/src/app/auth/registro/registro.component.ts
@@ -2,47 +2,49 @@ import { Component, OnInit } from '@angular/core';
 import { AbstractControl,FormControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from '../user.model';
+// import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.scss']
 })
-export class RegistroComponent implements OnInit{
-  userForm!: FormGroup;
-  user: User = {
-    id: 0,
-    username: '',
-    name: '',
-    email: '',
-    birth_date: '',
-    password: '',
-  };
+// export class RegistroComponent implements OnInit{
+//   userForm!: FormGroup;
+//   user: User = {
+//     id: 0,
+//     username: '',
+//     name: '',
+//     email: '',
+//     birth_date: '',
+//     password: '',
+//   };
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {}
+//   constructor(private formBuilder: FormBuilder, private router: Router) {}
 
-  ngOnInit() {
-    this.userForm = this.formBuilder.group({
-      username: ['', [Validators.required, Validators.minLength(4)]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['', [Validators.required, this.passwordMatchValidator]],
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      birth_date: ['', Validators.required]
-    }) as FormGroup; // Utilizamos el operador de aserción de tipo para indicar que userForm es de tipo FormGroup
-  }
+//   ngOnInit() {
+//     this.userForm = this.formBuilder.group({
+//       username: ['', [Validators.required, Validators.minLength(4)]],
+//       password: ['', [Validators.required, Validators.minLength(8)]],
+//       confirmPassword: ['', [Validators.required, this.passwordMatchValidator]],
+//       name: ['', Validators.required],
+//       email: ['', [Validators.required, Validators.email]],
+//       birth_date: ['', Validators.required]
+//     }) as FormGroup; // Utilizamos el operador de aserción de tipo para indicar que userForm es de tipo FormGroup
+//   }
   
 
-  passwordMatchValidator: ValidatorFn = (control: AbstractControl): { [key: string]: boolean } | null => {
-    const password = this.userForm?.get('password')?.value;
-    const confirmPassword = control.value;
+//   passwordMatchValidator: ValidatorFn = (control: AbstractControl): { [key: string]: boolean } | null => {
+//     const password = this.userForm?.get('password')?.value;
+//     const confirmPassword = control.value;
 
-    if (password !== confirmPassword) {
-      return { 'passwordMismatch': true };
-    }
+//     if (password !== confirmPassword) {
+//       return { 'passwordMismatch': true };
+//     }
 
-    return null;
-  };
+//     return null;
+//   };
 
   // onSubmit(event: Event): void {
   //   event.preventDefault();
@@ -95,19 +97,42 @@ export class RegistroComponent implements OnInit{
   //   }
   // }
 
-  get Mail(){
-    return this.userForm.get("email");
-  }
-  get Password(){
-    return this.userForm.get("password");
-  }
-  get Password2(){
-    return this.userForm.get("confirmPassword");
-  }
-  get Nombre(){
-    return this.userForm.get("name");
-  }
-  get Username(){
-    return this.userForm.get("username");
+//   get Mail(){
+//     return this.userForm.get("email");
+//   }
+//   get Password(){
+//     return this.userForm.get("password");
+//   }
+//   get Password2(){
+//     return this.userForm.get("confirmPassword");
+//   }
+//   get Nombre(){
+//     return this.userForm.get("name");
+//   }
+//   get Username(){
+//     return this.userForm.get("username");
+//   }
+// }
+
+export class RegistroComponent {
+  user = {
+    nombre: '',
+    email: '',
+    password: '',
+    confirm_password: ''
+  };
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    this.user.confirm_password = '';
+    this.http.post('http://localhost:8000/crear-usuario/', this.user).subscribe(
+      response => {
+        console.log('Usuario creado exitosamente', response);
+      },
+      error => {
+        console.error('Error al crear el usuario', error);
+      }
+    );
   }
 }
