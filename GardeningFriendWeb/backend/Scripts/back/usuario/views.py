68387e3b40746e1e349932ff7c3cafbffde3ea40
Cristian Valles <valles1710@gmail.com>
2024-05-05 19:27:52 -0300

    Se crea nueva Api rest

--- GardeningFriendWeb/backend/Scripts/back/usuario/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .models import CustomUser
-from .serializer import CustomUserSerializer
-
-class CreateUserView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = CustomUserSerializer
-# Create your views here.
