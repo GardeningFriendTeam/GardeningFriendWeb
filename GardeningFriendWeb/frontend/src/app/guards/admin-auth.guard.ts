6bf69539e394374a59d47f1934f0146817373381
TamiSorsaburu <tamysorsaburu@gmail.com>
2024-05-14 19:38:51 -0300

    Se agregra cambios en el servicio y guardia del frontend

---
+++ GardeningFriendWeb/frontend/src/app/guards/admin-auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root',
+  })
+  export class AdminGuard {
+    constructor(private authService: AuthService, private router: Router) {}
+  
+    canActivate(
+      route: ActivatedRouteSnapshot,
+      state: RouterStateSnapshot
+    ):
+      | Observable<boolean | UrlTree>
+      | Promise<boolean | UrlTree>
+      | boolean
+      | UrlTree {
+      if (this.authService.isLoggedIn() && this.authService.isAdmin()) {
+        return true;
+      } else {
+        return this.router.parseUrl('/access-denied');
+      }
+    }
+  }
+  
