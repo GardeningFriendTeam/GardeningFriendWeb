c5a6f30b88bd9db4c726448eaa8e8fd826be363d
TamiSorsaburu <tamysorsaburu@gmail.com>
2024-05-21 10:51:00 -0300

    feat: Se actualiza perfil de usuario para obtener username y email desde el backend

--- GardeningFriendWeb/backend/Scripts/back/perfil/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Perfil (models.Model):
-    nombre=models.CharField(max_length=50)
-    correo=models.CharField(max_length=50)
--- GardeningFriendWeb/frontend/src/app/auth/mi-cuenta/mi-cuenta.component.ts
+++ GardeningFriendWeb/frontend/src/app/auth/mi-cuenta/mi-cuenta.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { User } from '../user.model';
-import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-
-import { HttpClient } from '@angular/common/http';
-import { miCuenta } from 'src/app/services/miCuenta.service';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/auth/user.model';
 
 @Component({
   selector: 'app-mi-cuenta',
@@ -12,70 +9,18 @@ import { miCuenta } from 'src/app/services/miCuenta.service';
   styleUrls: ['./mi-cuenta.component.scss']
 })
 export class MiCuentaComponent {
+  userSubject!: User;
+  userSub!: Subscription;
 
-  perfil: any={}; 
-  constructor(private servicio:miCuenta,private activatedRouter: ActivatedRoute,private formBuilder: FormBuilder, private router: Router, private http: HttpClient) { }
-   id = this.activatedRouter.snapshot.params['id'];
-  ngOnInit() {
-    this.servicio.getPerfil(1).subscribe(dato=>{
-      this.perfil=dato;
-      console.log("funciona desde el ngonInit  "+this.perfil.nombre);
-      console.log("funciona desde el ngonInit  "+this.perfil.correo);
-      
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.userSub = this.authService.userSubject.subscribe((data) => {
+      this.userSubject = data || {};
     });
-    
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }
-  // constructor(private formBuilder: FormBuilder, private router: Router) {}
-
-  // ngOnInit() {
-    // this.usersService.getUsers().subscribe((data: User[]) => {
-    //   this.myUser = data;
-    //   console.log("Mi cuenta user:", this.myUser);
-    // })
-
-    // this.userForm = this.formBuilder.group({
-    //   username: ['', [Validators.required, Validators.minLength(4)]],
-    //   password: ['', [Validators.required, Validators.minLength(8)]],
-    //   confirmPassword: ['', [Validators.required, this.passwordMatchValidator]],
-    //   name: ['', Validators.required],
-    //   email: ['', [Validators.required, Validators.email]],
-    //   birth_date: ['', Validators.required]
-    // });
-
-  // }
-
-  // passwordMatchValidator: ValidatorFn = (control: AbstractControl): { [key: string]: boolean } | null => {
-  //   const password = this.userForm?.get('password')?.value;
-  //   const confirmPassword = control.value;
-
-  //   if (password !== confirmPassword) {
-  //     return { 'passwordMismatch': true };
-  //   }
-
-  //   return null;
-  // };
-
-  // onSubmit(event: Event, user: User) {
-  //   if (this.userForm.valid) {
-  //     console.log(this.userForm.value);
-  //     this.router.navigateByUrl('');
-  //     this.usersService.updateUser(user).subscribe(
-  //       data => {
-  //         console.log(data.id);
-  //       })
-  //     this.userForm.reset();
-  //   } else {
-  //     this.userForm.markAllAsTouched();
-  //   }
-  // }
-
-  // updateUser() {
-  //   if (this.user) {
-  //     this.usersService.updateUser(this.user).subscribe(
-  //       updatedUser => console.log('User updated:', updatedUser),
-  //       error => console.log('Error updating user:', error)
-  //     );
-  //   }
-  // }
-// }
