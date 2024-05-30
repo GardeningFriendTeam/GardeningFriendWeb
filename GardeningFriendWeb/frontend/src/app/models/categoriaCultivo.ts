4197978f9ff075a816e2554bd7678b8e85ea0f14
mariquena allosa <allosamariquena@gmail.com>
2024-05-30 11:30:04 -0300

    Modificar tarjetas de cultivos y consumir categoria de cultivos

--- GardeningFriendWeb/frontend/src/app/models/categoriaCultivo.ts
+++ GardeningFriendWeb/frontend/src/app/models/categoriaCultivo.ts
@@ -1,8 +1,8 @@
 export class CategoriaCultivo{
     public id?: number;
-    nombre?: string;
+    nombre: string;
 
-    constructor(nombre:string){
-        this.nombre=nombre;
+    constructor(nombre: string){
+        this.nombre = nombre;
     }
 }
