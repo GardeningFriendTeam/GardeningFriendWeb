218c9194f8b7ef374f733b371279639e80037c92
mariquena allosa <allosamariquena@gmail.com>
2024-05-30 21:57:19 -0300

    Modificar logica de creacion de cultivo

--- GardeningFriendWeb/frontend/src/app/admin/CRUD/agregar-cultivo/agregar-cultivo.component.ts
+++ GardeningFriendWeb/frontend/src/app/admin/CRUD/agregar-cultivo/agregar-cultivo.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { CultivosService } from 'src/app/services/cultivos.service';
-import { Cultivo } from 'src/app/enciclopedia/cultivos/cultivos.model';
 import { CategoriaCultivo } from 'src/app/models/categoriaCultivo';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-agregar-cultivo',
@@ -10,95 +10,84 @@ import { CategoriaCultivo } from 'src/app/models/categoriaCultivo';
   styleUrls: ['./agregar-cultivo.component.scss']
 })
 export class AgregarCultivoComponent {
-  cultivos: any = {};
-  categorias: any = {};
-
+  categorias: CategoriaCultivo[] = [];
   nombre: string = "";
-  categoria: any;
+  categoriaNombre?: string; // Guardar el nombre de la categoría seleccionada
   descripcion: string = "";
-  imagen!: File;
+  imagen: File | null = null;
   region: string = "";
   estacion: string = "";
-  temperatura: string = "";
+  temperatura: number | null = null;
+  favorito: boolean = false;
 
-  constructor(private cultivoService: CultivosService, private router: Router){
-
-  }
+  constructor(private cultivoService: CultivosService, private router: Router) {}
 
   ngOnInit(): void {
     this.cultivoService.traerCategorias().subscribe(resp2 => {
       this.categorias = resp2;
-
-    })
-   }
-
-  // create(): void{
-  //   const cultivo = new FormData();
-  //   cultivo.append('nombre',this.nombre)
-  //   this.cultivoService.create(cultivo).subscribe(
-  //     data=>this.router.navigate(['/verCultivo'])
-  //   );
-  //   console.log(cultivo);
-  // }
+    });
+  }
 
   guardarNombre(event: any) {
-    console.log(this.nombre = event.target.value)
-  }
-
-  guardarCategoria(event: any) {
-    console.log(this.categoria = event.target.value)
-  }
-
-  guardarDescripcion(event: any) {
-    console.log(this.descripcion = event.target.value)
-  }
-
-  enviarFoto(event: any) {
-    console.log(this.imagen = event.target.files[0])
-  }
-
-  guardarRegion(event: any) {
-    console.log(this.region = event.target.value)
-  }
-
-  guardarEstacion(event: any) {
-    console.log(this.estacion = event.target.value)
-  }
-
-  guardarTemperatura(event: any) {
-    console.log(this.temperatura = event.target.value)
+    this.nombre = event.target.value;
   }
 
   selectCategoria(event: any) {
-    console.log(this.categoria = event.target.value)
+    this.categoriaNombre = event.target.value; // Guardar el nombre de la categoría
   }
 
-  
+  guardarDescripcion(event: any) {
+    this.descripcion = event.target.value;
+  }
 
+  enviarFoto(event: any) {
+    this.imagen = event.target.files[0] || null;
+  }
 
+  guardarRegion(event: any) {
+    this.region = event.target.value;
+  }
 
+  guardarEstacion(event: any) {
+    this.estacion = event.target.value;
+  }
 
+  guardarTemperatura(event: any) {
+    this.temperatura = event.target.value !== "" ? parseFloat(event.target.value) : null;
+  }
 
   create() {
+    if (!this.categoriaNombre) {
+      console.log('Categoría no seleccionada');
+      return;
+    }
     const cult = new FormData();
     cult.append('nombre', this.nombre);
-    cult.append('categoria', this.categoria);
+    cult.append('categoria', JSON.stringify({ nombre: this.categoriaNombre }));
     cult.append('descripcion', this.descripcion);
-    cult.append('imagen', this.imagen, this.imagen!.name);
+    cult.append('imagen', this.imagen || "");
     cult.append('region', this.region);
     cult.append('estacion', this.estacion);
-    cult.append('temperatura', this.temperatura);
-    
-    
+    cult.append('temperatura', this.temperatura !== null ? this.temperatura.toString() : "");
+    cult.append('favorito', this.favorito.toString());
+
     this.cultivoService.create(cult).subscribe(
-      cultivo => this.router.navigate(['/administrarCultivos'])
-      ,
-      error => console.log(error)
-
+      cultivo => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Cultivo creado',
+          text: 'El cultivo se ha creado con éxito',
+        });
+        this.router.navigate(['/administrarCultivos']);
+      },
+      error => {
+        console.log(error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Hubo un problema al crear el cultivo',
+        });
+      }
     );
-
-
-
   }
-
 }
